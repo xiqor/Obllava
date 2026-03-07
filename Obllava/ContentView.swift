2cd5f1d3fb0695cf2a49a07a79b218ef718d7806
Max <xiqor@list.ru>
2026-03-07 19:07:56 +0300

    Initial Commit

---
+++ Obllava/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Obllava
+//
+//  Created by МФ on 7/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Obllava/ObllavaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObllavaApp.swift
+//  Obllava
+//
+//  Created by МФ on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ObllavaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
